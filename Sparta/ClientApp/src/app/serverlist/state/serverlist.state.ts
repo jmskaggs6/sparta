9f30cf9cd79f429fdd7c72cd3c58fa06833e3fee
Jeremy <jeremy@inventiv.com>
2019-12-08 14:59:34 -0600

    add serverlist state

--- Sparta/ClientApp/src/app/serverlist/state/serverlist.state.ts
+++ Sparta/ClientApp/src/app/serverlist/state/serverlist.state.ts
@@ -1,17 +1,46 @@
-import { Store, State } from '@ngxs/store';
+import { Store, State, Action, StateContext, Selector } from '@ngxs/store';
 import { ServerListStartUp } from '../models/serverlist.model';
+import { FetchAllServers } from './serverlist.action';
+import { Repository } from '../repository';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export interface ServerListStateModel {
     serverListStartup: ServerListStartUp[];
-    currentServer: string;
+    currentSelectedServer: string;
 }
 
 @State<ServerListStateModel>({
-    name: 'zoo',
+    name: 'serverlist',
     defaults: {
       serverListStartup: [],
-      currentServer: ""
+      currentSelectedServer: ""
     }
   })
   export class ServerListState {
+    constructor(private repo: Repository) {
+
+    }
+
+    @Selector()
+    static getServerList(state: ServerListStateModel) {
+        return state.serverListStartup;
+    }
+
+    @Action(FetchAllServers)
+    public fetchAllServers(ctx: StateContext<ServerListStateModel>, action: FetchAllServers) 
+    {
+        const state = ctx.getState(); // always returns the freshest slice of state
+        return this.repo.getServerList().pipe(
+          tap((result) => {
+              if (result != undefined || result != null)
+              {
+                  ctx.setState({
+                      ...state,
+                      serverListStartup: result
+                  });
+              }
+      }));
+    }
+    
   }

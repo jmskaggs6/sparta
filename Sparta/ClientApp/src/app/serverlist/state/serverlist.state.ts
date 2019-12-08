47524f4beef376be082e1ea37a1e32f5ea29ab26
Jeremy <jeremy@inventiv.com>
2019-12-07 21:28:47 -0600

    serverlist state

--- Sparta/ClientApp/src/app/serverlist/state/serverlist.state.ts
+++ Sparta/ClientApp/src/app/serverlist/state/serverlist.state.ts
@@ -1,2 +1,17 @@
-import { Store } from '@ngxs/store';
-import { } from '../models/serverlist.model';
+import { Store, State } from '@ngxs/store';
+import { ServerListStartUp } from '../models/serverlist.model';
+
+export interface ServerListStateModel {
+    serverListStartup: ServerListStartUp[];
+    currentServer: string;
+}
+
+@State<ServerListStateModel>({
+    name: 'zoo',
+    defaults: {
+      serverListStartup: [],
+      currentServer: ""
+    }
+  })
+  export class ServerListState {
+  }

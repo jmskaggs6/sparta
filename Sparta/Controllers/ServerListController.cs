d7c6a6d3db6727d901eb3de4738984f7b5f8c66a
Jeremy <jeremy@inventiv.com>
2019-12-08 00:26:45 -0600

    back end work

---
+++ Sparta/Controllers/ServerListController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sparta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sparta.Controllers
+{
+    [ApiController]
+    public class ServerListController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ServerListController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentException(nameof(mediator));
+        }
+
+        public async Task<ActionResult<ServerListStartup>> GetServerListStart()
+        {
+            //var servers = await _mediator.Send(new x());
+
+            return Ok();
+        }
+    }
+}
---
+++ Sparta/Queries/GetServerListStartup.cs
@@ -0,0 +1,19 @@
+﻿using Sparta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace Sparta.Queries
+{
+    public class GetServerListStartup : QueryBase<ServerListStartup>
+    {
+        private readonly IConfiguration _configuration;
+        public GetServerListStartup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
